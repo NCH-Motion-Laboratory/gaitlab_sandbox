865911de381644b98f586ea0825dbee042eab98d
Jussi Nurminen <jnu@ki.fi>
2015-04-23 11:35:36 +0300

    generic plotter

---
+++ nexus_py/vicon_plot.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 11:31:09 2015
+
+@author: jussi
+
+generic plotter 
+
+passed params (corresponds to difference between plotting scripts:)
+-layout
+-data to plot
+-plot main title
+-create pdf or not (pdf name)
+
+"""
+
+
+
