ec3367f018c6083bb84fb9091537368b0d9525ab
Jussi Nurminen <jnu@iki.fi>
2015-04-30 11:34:04 +0300

    nexus plotter class ok (not tested)

--- nexus_py/nexus_plot.py
+++ nexus_py/nexus_plot.py
@@ -48,27 +48,17 @@ def strip_ws(str):
     """ Remove spaces from string """
     return str.replace(' ','')
 
-
 class nexus_plotter():
-
     """ Create a plot of Nexus variables. Can overlay data from several trials. """
-
-    #layout, plotvars, plotheightratios=None, maintitlestr='Plot for ', makepdf=False, pdftitlestr=None,
-#                onesided_kinematics=False, annotate_disconnected=True, annotate_reused=True):
 
     def __init__(self, layout, plotvars):
         """ Sets plot layout and variables to be read. """
         
         # default parameters, if none specified on cmd line or config file
-        emg_passband = None   # none for no filtering, or [f1,f2] for bandpass
-        side = None   # will autodetect unless specified
-
+        self.emg_passband = None   # none for no filtering, or [f1,f2] for bandpass
+        self.side = None   # will autodetect unless specified
+        self.trialname = None
         self.plotvars = plotvars        
-        self.plotheightratios = plotheightratios
-        self.maintitlestr = maintitlestr
-        self.makepdf = makepdf
-        self.pdftitlestr = pdftitlestr
-        self.onesided_kinematics = onesided_kinematics
         
         # paths
         pathprefix = 'c:/users/' + getpass.getuser()
@@ -84,7 +74,7 @@ class nexus_plotter():
         arglist += sys.argv[1:]  # add cmd line arguments    
         arglist = [strip_ws(x) for x in arglist]  # rm whitespace
         arglist = [x for x in arglist if x and x[0] != '#']  # rm comments
-        emg_mapping = {}
+        self.emg_mapping = {}
         for arg in arglist:
             eqpos = arg.find('=')
             if eqpos < 2:
@@ -93,195 +83,199 @@ class nexus_plotter():
                 key = arg[:eqpos]
                 val = arg[eqpos+1:]
                 if key.lower() == 'side':
-                    side = val.upper()
+                    self.side = val.upper()
                 elif key.lower() == 'emg_passband':
                     try:
-                        emg_passband = [float(x) for x in val.split(',')]   
+                        self.emg_passband = [float(x) for x in val.split(',')]   
                     except ValueError:
                         error_exit('Invalid EMG passband. Specify as emg_passband=f1,f2')
                 else:
                     # assume it's EMG channel remapping
-                    emg_mapping[key] = val
+                    self.emg_mapping[key] = val
                 
         # locate PiG normal data
-        gcdpath = 'normal.gcd'
+        self.gcdpath = 'normal.gcd'
         # check user's desktop also
-        if not os.path.isfile(gcdpath):
-            gcdpath = desktop + '/projects/llinna/nexus_py/normal.gcd'
-        if not os.path.isfile(gcdpath):
+        if not os.path.isfile(self.gdcpath):
+            self.gdcpath = desktop + '/projects/llinna/nexus_py/normal.gcd'
+        if not os.path.isfile(self.gdcpath):
             error_exit('Cannot find Plug-in Gait normal data (normal.gcd)')
         
         # set default plotting parameters
         # figure size
-        totalfigsize = (14,12)
+        self.totalfigsize = (14,12)
         # grid dimensions, vertical and horizontal
         self.gridv = layout[0]
         self.gridh = layout[1]
-        if not plotheightratios:
-            plotheightratios = [1] * gridv
-        # main title
-        maintitle = maintitlestr + trialname + ' ('+side+')'
         # trace colors, right and left
-        tracecolor_r = 'lawngreen'
-        tracecolor_l = 'red'
+        self.tracecolor_r = 'lawngreen'
+        self.tracecolor_l = 'red'
         # label font size
-        fsize_labels=10
+        self.fsize_labels=10
         # for plotting kinematics / kinetics normal data
-        normals_alpha = .3
-        normals_color = 'gray'
+        self.normals_alpha = .3
+        self.normals_color = 'gray'
         # emg normals
-        emg_normals_alpha = .3
-        emg_normals_color = 'red'
-        emg_ylabel = 'mV'
-      
-
+        self.emg_normals_alpha = .3
+        self.emg_normals_color = 'red'
+        self.emg_ylabel = 'mV'
+        # x label
+        self.xlabel = ''
+                                                      
     def open_trial(self, trialpath=None):
-        """ Read specified trial, or the one opened in Nexus. """
+        """ Read specified trial, or the one already opened in Nexus. """
 
         # connect to Nexus
         vicon = ViconNexus.ViconNexus()
 
         if trialpath:
             vicon.OpenTrial(trialpath, 30)            
+            # TODO: check errors
         
         subjectnames = vicon.GetSubjectNames()  
         if not subjectnames:
             error_exit('No subject')
-        subjectname = subjectnames[0]
+        
         trialname_ = vicon.GetTrialName()
         if not trialname_:
             error_exit('No trial loaded')
-        sessionpath = trialname_[0]
-        trialname = trialname_[1]
-      
+        self.sessionpath = trialname_[0]
+        self.trialname = trialname_[1]
+        self.subjectname = subjectnames[0]
+        
         # try to detect side (L/R)
         vgc = nexus_getdata.gaitcycle(vicon)
-        if not side:
-            side = vgc.detect_side(vicon)
-    
-        self.pig = nexus_getdata.pig_outputs()
-        self.emg = nexus_getdata.nexus_emg(mapping_changes=emg_mapping)
+        if not self.side:
+            self.side = vgc.detect_side(vicon)
     
         # will read EMG/PiG data only if necessary
+        self.pig = nexus_getdata.pig_outputs()
+        self.emg = nexus_getdata.nexus_emg(mapping_changes=self.emg_mapping)
         read_emg = False
         read_pig = False
-        
-        # check the variables to see what should be read
         emg_plot_chs = []
         emg_plot_pos = []
-        pig_plot_vars = []
+        self.pig_plot_vars = []
         pig_plot_pos = []
-        
         for i, var in enumerate(self.plotvars):
             if var == None:
                 pass
             else:
                 if var[0] == 'X':
-                    var = side + var[1:]  # autodetect side
-                if emg.is_logical_channel(var):
+                    var = self.side + var[1:]  # autodetect side
+                if self.emg.is_logical_channel(var):
                     read_emg = True
                     emg_plot_chs.append(var)
                     emg_plot_pos.append(i)
-                elif pig.is_pig_variable(var):
+                elif self.pig.is_pig_variable(var):
                     read_pig = True
-                    pig_plot_vars.append(var)
+                    self.pig_plot_vars.append(var)
                     pig_plot_pos.append(i)
                 else:
                     error_exit('Unknown variable: ' + var)
-    
         if read_emg:
-            emg.read(vicon)
+            self.emg.read(vicon)
         if read_pig:
-            pig.read(vicon, 'PiGLB', gcdpath)
- 
+            self.pig.read(vicon, 'PiGLB', self.gcdpath)
+
 
     def plot_trial(self, plotheightratios=None, maintitlestr='Plot for ',
-                 makepdf=False, pdftitlestr=None, onesided_kinematics=False):
-
-        """ Plot active trial (call open_trial first). If a plot is already 
+                 makepdf=False, pdftitlestr='Nexus_plot_', onesided_kinematics=False):
+        """ Plot active trial (must call open_trial first). If a plot is already 
         active, the new trial will be overlaid on the previous one."""        
+
+        if not self.trialname:
+            raise Exception('No trial loaded')
         
         # output filename
         if makepdf:
-            pdf_name = sessionpath + pdftitlestr + trialname + '.pdf'
+            pdf_name = self.sessionpath + pdftitlestr + self.trialname + '.pdf'
          
-        xlabel = ''
-        
-        if side == 'L':
-            tracecolor = tracecolor_l
+        if self.side == 'L':
+            tracecolor = self.tracecolor_l
         else:
-            tracecolor = tracecolor_r
+            tracecolor = self.tracecolor_r
+
+        # if plot height ratios not set, set them all equal    
+        if not plotheightratios:
+            self.plotheightratios = [1] * self.gridv
+
+        maintitle = maintitlestr + self.trialname + ' ('+self.side+')'
         
-        # for kinematics / kinetics: 0,1...100
+        # x variable for kinematics / kinetics: 0,1...100
         tn = np.linspace(0, 100, 101)
         # for normal data: 0,2,4...100.
         tn_2 = np.array(range(0, 101, 2))
-            
-        fig = plt.figure(figsize=totalfigsize)
-        gs = gridspec.GridSpec(gridv, gridh, height_ratios=plotheightratios)
-        plt.suptitle(maintitle, fontsize=12, fontweight="bold")
-        #plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.5, hspace=0.5)
         
-        if pig_plot_vars:
-            for k, var in enumerate(pig_plot_vars):
-                plt.subplot(gs[pig_plot_pos[k]])
-                varname_full = 'Norm'+side+var
+        # if figure already exists, use it for overlay, otherwise create a new one
+        if not self.fig:
+            self.fig = plt.figure(figsize=self.totalfigsize)
+            self.gs = gridspec.GridSpec(self.gridv, self.gridh, height_ratios=plotheightratios)
+        else:
+            plt.figure(self.fig.number)
+
+        plt.suptitle(maintitle, fontsize=12, fontweight="bold")
+        
+        if self.pig_plot_vars:
+            for k, var in enumerate(self.pig_plot_vars):
+                plt.subplot(self.gs[self.pig_plot_pos[k]])
+                varname_full = 'Norm'+self.side+var
                 # whether to plot two-sided kinematics                
-                if not pig.is_kinetic_var(var) and not onesided_kinematics:
+                if not self.pig.is_kinetic_var(var) and not onesided_kinematics:
                     varname_r = 'Norm' + 'R' + var
                     varname_l = 'Norm' + 'L' + var
-                    plt.plot(tn, pig.Vars[varname_r], tracecolor_r)
-                    plt.plot(tn, pig.Vars[varname_l], tracecolor_l)
+                    plt.plot(tn, self.pig.Vars[varname_r], self.tracecolor_r)
+                    plt.plot(tn, self.pig.Vars[varname_l], self.tracecolor_l)
                 else:
-                    plt.plot(tn, pig.Vars[varname_full], tracecolor)
-                nor = np.array(pig.normaldata(var))[:,0]
-                nstd = np.array(pig.normaldata(var))[:,1]
-                title = pig.description(var)
-                ylabel = pig.ylabel(varname_full)
-                plt.fill_between(tn_2, nor-nstd, nor+nstd, color=normals_color, alpha=normals_alpha)
-                plt.title(title, fontsize=fsize_labels)
-                plt.xlabel(xlabel,fontsize=fsize_labels)
-                plt.ylabel(ylabel, fontsize=fsize_labels)
+                    plt.plot(tn, self.pig.Vars[varname_full], tracecolor)
+                nor = np.array(self.pig.normaldata(var))[:,0]
+                nstd = np.array(self.pig.normaldata(var))[:,1]
+                title = self.pig.description(var)
+                ylabel = self.pig.ylabel(varname_full)
+                plt.fill_between(tn_2, nor-nstd, nor+nstd, color=self.normals_color, alpha=self.normals_alpha)
+                plt.title(title, fontsize=self.fsize_labels)
+                plt.xlabel(self.xlabel,fontsize=self.fsize_labels)
+                plt.ylabel(ylabel, fontsize=self.fsize_labels)
                 #plt.ylim(kinematicsymin[k], kinematicsymax[k])
                 plt.axhline(0, color='black')  # zero line
                 plt.locator_params(axis = 'y', nbins = 6)  # reduce number of y tick marks
         
-        if emg_plot_chs:
-            for k, thisch in enumerate(emg_plot_chs):
+        if self.emg_plot_chs:
+            for k, thisch in enumerate(self.emg_plot_chs):
                 side_this = thisch[0]
                 # choose EMG data normalized according to side
                 if side_this == 'L':
-                    tn_emg = emg.tn_emg_l
-                    emgdata = emg.logical_data_gc1l
-                    emg_yscale = emg.yscale_gc1l
+                    tn_emg = self.emg.tn_emg_l
+                    emgdata = self.emg.logical_data_gc1l
+                    emg_yscale = self.emg.yscale_gc1l
                 elif side_this == 'R':
-                    tn_emg = emg.tn_emg_r
-                    emgdata = emg.logical_data_gc1r
-                    emg_yscale = emg.yscale_gc1r
+                    tn_emg = self.emg.tn_emg_r
+                    emgdata = self.emg.logical_data_gc1r
+                    emg_yscale = self.emg.yscale_gc1r
                 else:
                     error_exit('Unexpected EMG channel name: ', thisch)
-                ax=plt.subplot(gs[emg_plot_pos[k]])
+                ax=plt.subplot(self.gs[self.emg_plot_pos[k]])
                 if emgdata[thisch] == 'EMG_DISCONNECTED':
                     ax.annotate('disconnected', xy=(50,0), ha="center", va="center")   
                 elif emgdata[thisch] == 'EMG_REUSED':
                         ax.annotate('reused', xy=(50,0), ha="center", va="center")
                 else:
-                    plt.plot(tn_emg, 1e3*emg.filter(emgdata[thisch], emg_passband), 'black')
-                chlabel = emg.ch_labels[thisch]
+                    plt.plot(tn_emg, 1e3*self.emg.filter(emgdata[thisch], self.emg_passband), 'black')
+                chlabel = self.emg.ch_labels[thisch]
                 # plot EMG normal bars
-                emgbar_ind = emg.ch_normals[thisch]
+                emgbar_ind = self.emg.ch_normals[thisch]
                 for k in range(len(emgbar_ind)):
                     inds = emgbar_ind[k]
-                    plt.axvspan(inds[0], inds[1], alpha=emg_normals_alpha, color=emg_normals_color)    
+                    plt.axvspan(inds[0], inds[1], alpha=self.emg_normals_alpha, color=self.emg_normals_color)    
                 plt.ylim(-1e3*emg_yscale[thisch], 1e3*emg_yscale[thisch])  # scale from logical channel
                 plt.xlim(0,100)
                 plt.title(chlabel, fontsize=10)
-                plt.xlabel(xlabel, fontsize=fsize_labels)
-                plt.ylabel(emg_ylabel, fontsize=fsize_labels)
+                plt.xlabel(self.xlabel, fontsize=self.fsize_labels)
+                plt.ylabel(self.emg_ylabel, fontsize=self.fsize_labels)
                 plt.locator_params(axis = 'y', nbins = 4)
         
         # fix plot spacing, restrict to below title
-        gs.tight_layout(fig, h_pad=.5, w_pad=.5, rect=[0,0,1,.95])  
+        self.gs.tight_layout(self.fig, h_pad=.5, w_pad=.5, rect=[0,0,1,.95])  
     
         #call plt.show() externally after all figures are complete - it blocks
         #plt.show()
@@ -290,7 +284,7 @@ class nexus_plotter():
         if makepdf:
             with PdfPages(pdf_name) as pdf:
                 print("Writing "+pdf_name)
-                pdf.savefig(fig)
+                pdf.savefig(self.fig)
         
         
     
