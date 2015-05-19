e5c105ba4b55a4650ff82c64012ef9cfcaba623a
Jussi Nurminen <jnu@iki.fi>
2015-05-19 14:06:03 +0300

    bandpass on kinetics-emg title

--- nexus_py/nexus_kinetics_emgplot.py
+++ nexus_py/nexus_kinetics_emgplot.py
@@ -13,6 +13,7 @@ from nexus_plot import nexus_plotter
 layout = [8,3]
 plotheightratios = [3,2,2,3,2,2,2,3]
 pdf_prefix = 'Kinetics_EMG_'
+maintitleprefix='Kinetics-EMG plot for '
 
 nplotter = nexus_plotter(layout)
 
@@ -30,7 +31,12 @@ plotvars = ['HipAnglesX','KneeAnglesX','AnkleAnglesX',
             'HipPowerZ','KneePowerZ','AnklePowerZ']
 
 nplotter.open_trial(nexusvars=plotvars)
-nplotter.plot_trial(plotheightratios=plotheightratios, maintitleprefix='Kinetics-EMG plot for ')
+
+trialname = nplotter.trialname
+maintitle = maintitleprefix + trialname
+maintitle = maintitle + '\n' + nplotter.get_emg_filter_description()
+
+nplotter.plot_trial(plotheightratios=plotheightratios, maintitle=maintitle)
 nplotter.create_pdf(pdf_prefix=pdf_prefix)
 
 nplotter.show()
