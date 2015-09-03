688395bba12db658fcbae6cba78046f902a09422
Jussi Nurminen <jnu@iki.fi>
2015-09-03 11:41:50 +0300

    overlay wip

---
+++ nexus_py/c3d_emg_overlay.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 11:37:51 2015
+
+C3D EMG overlay plot. wip
+
+@author: Jussi
+"""
+
+from gait_plot import gaitplotter
+import sys
+from gait_getdata import error_exit
+import gaitplotter_plots
+
+layout = [9,2]
+pdftitlestr = 'EMG_overlay_'
+emgcolors = ['black','blue','gray']
+plotvars = gaitplotter_plots.overlay_emg
+MAX_TRIALS = 3
+
+nplotter = gaitplotter(layout)
+nplotter.annotate_disconnected = False
+maintitle = 'EMG overlay plot' + '\n' + nplotter.get_emg_filter_description()
+chosen = nplotter.c3d_trialselector()
+
+if len(chosen) > MAX_TRIALS:
+    error_exit('Too many trials selected for the overlay plot!')
+  
+for i,trialpath in enumerate(chosen):
+    nplotter.open_c3d_trial(trialpath)
+    nplotter.read_trial(plotvars)
+    nplotter.plot_trial(maintitle=maintitle,
+                        emg_tracecolor=emgcolors[i])
+    
+nplotter.show()
+
