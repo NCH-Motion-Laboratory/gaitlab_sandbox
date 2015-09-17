0ce23a012f3a003ac695d8392dfa3b2c57dce5ae
Jussi Nurminen <jnu@iki.fi>
2015-09-17 14:05:27 +0300

    fix Nexus muscle length plot

--- nexus_py/nexus_musclelen_plot.py
+++ nexus_py/nexus_musclelen_plot.py
@@ -16,13 +16,12 @@ layout = [3,3]
 maintitleprefix='Muscle length plot for '
 pdftitlestr='muscle_length_'
 
-gplotter = gaitplotter(layout)
+gplotter = gaitplotter()
 plotvars = gp.layouts.std_musclelen
 gplotter.open_nexus_trial()
 gplotter.read_trial(plotvars)
 trialname = gplotter.trial.trialname
 maintitle = maintitleprefix + trialname
-maintitle = maintitle + '\n' + gplotter.get_emg_filter_description()
 gplotter.plot_trial(maintitle=maintitle)
 gplotter.create_pdf(pdf_prefix=pdftitlestr)
 
