005791cb6004278c9dec1a01cb3c632fd3d68a96
Jussi Nurminen <jnu@iki.fi>
2015-10-23 10:43:00 +0300

    automark with subject-specific velocities

--- nexus_py/nexus_automark.py
+++ nexus_py/nexus_automark.py
@@ -17,8 +17,9 @@ import numpy as np
 from scipy import signal
 import ViconNexus
 import matplotlib.pyplot as plt
+from gp import getdata
 
-THRESHOLD_FALL = .1  # above minimum
+THRESHOLD_FALL = .2  # above minimum
 THRESHOLD_UP = .5
 
 vicon = ViconNexus.ViconNexus()
@@ -50,6 +51,17 @@ def roi_pos_vel_acc(marker):
     Vroi = np.gradient(Proi)[0]
     Aroi = np.gradient(Vroi)[0]
     return roifr, Proi, Vroi, Aroi
+    
+def get_fp_strike_and_toeoff():
+    """ Return forceplate strike and toeoff frames. """
+    FP_THRESHOLD = .1  # % of maximum force
+    fp0 = getdata.forceplate(vicon)
+    ftot = fp0.forcetot
+    frel = ftot/ftot.max()
+    # in analog frames
+    fpstrike = rising_zerocross(frel-FP_THRESHOLD)[0]
+    fptoeoff = falling_zerocross(frel-FP_THRESHOLD)[0]
+    return np.round(fpstrike/fp0.samplesperframe), np.round(fptoeoff/fp0.samplesperframe)
      
 # get data for specified markers
 mrkdata = {}    
@@ -61,39 +73,64 @@ for marker in ['RHEE','RTOE','RANK','LHEE','LTOE','LANK']:
     mrkdata[marker+'_A'] = A
 roi0 = roifr[0]
 
-print('Autodetect right:')
-# compute foot centre velocity
-footctrV = (mrkdata['RHEE_V']+mrkdata['RTOE_V']+mrkdata['RANK_V'])/3.
-rfootctrv = np.sqrt(np.sum(footctrV[:,1:3]**2,1))
-# find local minima below zero
-rng = rfootctrv.max()-rfootctrv.min()
-thre_fall = rng * THRESHOLD_FALL + rfootctrv.min()
-thre_up = rng * THRESHOLD_UP + rfootctrv.min()
-fallframes = falling_zerocross(rfootctrv-thre_fall)
-upframes = rising_zerocross(rfootctrv-thre_up)
-print(fallframes+roi0)
-for strike in fallframes:
+rfootctrV = (mrkdata['RHEE_V']+mrkdata['RTOE_V']+mrkdata['RANK_V'])/3.
+rfootctrv = np.sqrt(np.sum(rfootctrV[:,1:3]**2,1))
+lfootctrV = (mrkdata['LHEE_V']+mrkdata['LTOE_V']+mrkdata['LANK_V'])/3.
+lfootctrv = np.sqrt(np.sum(lfootctrV[:,1:3]**2,1))
+
+
+print('Initial strike autodetect right:')
+thre_fall = rfootctrv.max() * THRESHOLD_FALL + rfootctrv.min()
+thre_up = rfootctrv.max() * THRESHOLD_UP + rfootctrv.min()
+rfallframes = falling_zerocross(rfootctrv-thre_fall)
+rupframes = rising_zerocross(rfootctrv-thre_up)
+print(rfallframes+roi0)
+
+print('Initial strike autodetect left:')
+thre_fall = lfootctrv.max() * THRESHOLD_FALL + lfootctrv.min()
+thre_up = lfootctrv.max() * THRESHOLD_UP + lfootctrv.min()
+lfallframes = falling_zerocross(lfootctrv-thre_fall)
+lupframes = rising_zerocross(lfootctrv-thre_up)
+print(lfallframes+roi0)
+
+fpstrike, fptoeoff = get_fp_strike_and_toeoff()
+print('Forceplate strike:', fpstrike, 'toeoff:', fptoeoff)
+print('Relative velocities at forceplate:')
+if min(abs(lfallframes-(fpstrike-roi0))) < min(abs(rfallframes-(fpstrike-roi0))):
+    print('Left:')
+    print('Strike:',lfootctrv[fpstrike-roi0]/lfootctrv.max())
+    print('Toeoff:',lfootctrv[fptoeoff-roi0]/lfootctrv.max())
+    THRESHOLD_FALL = lfootctrv[fpstrike-roi0]/lfootctrv.max()
+    THRESHOLD_UP = lfootctrv[fptoeoff-roi0]/lfootctrv.max()
+else:
+    print('Right:')
+    print('Strike:',rfootctrv[fpstrike-roi0]/rfootctrv.max())
+    print('Toeoff:',rfootctrv[fptoeoff-roi0]/rfootctrv.max())
+    THRESHOLD_FALL = rfootctrv[fpstrike-roi0]/rfootctrv.max()
+    THRESHOLD_UP = rfootctrv[fptoeoff-roi0]/rfootctrv.max()
+
+print('Redetect strike and toeoff right:')
+thre_fall = rfootctrv.max() * THRESHOLD_FALL + rfootctrv.min()
+thre_up = rfootctrv.max() * THRESHOLD_UP + rfootctrv.min()
+rfallframes = falling_zerocross(rfootctrv-thre_fall)
+rupframes = rising_zerocross(rfootctrv-thre_up)
+print(rfallframes+roi0)
+print('Redetect strike and toeoff left:')
+thre_fall = lfootctrv.max() * THRESHOLD_FALL + lfootctrv.min()
+thre_up = lfootctrv.max() * THRESHOLD_UP + lfootctrv.min()
+lfallframes = falling_zerocross(lfootctrv-thre_fall)
+lupframes = rising_zerocross(lfootctrv-thre_up)
+print(lfallframes+roi0)
+
+# mark events
+for strike in rfallframes:
     vicon.CreateAnEvent(subjectname, 'Right', 'Foot Strike', strike+roi0, 0.0 )
-for fr in upframes:
+for fr in rupframes:
     vicon.CreateAnEvent(subjectname, 'Right', 'Foot Off', fr+roi0, 0.0 )
-
-
-print('Autodetect left:')
-# compute foot centre velocity
-footctrV = (mrkdata['LHEE_V']+mrkdata['LTOE_V']+mrkdata['LANK_V'])/3.
-lfootctrv = np.sqrt(np.sum(footctrV[:,1:3]**2,1))
-# find local minima below zero
-rng = lfootctrv.max()-lfootctrv.min()
-thre_fall = rng * THRESHOLD_FALL + lfootctrv.min()
-thre_up = rng * THRESHOLD_UP + lfootctrv.min()
-fallframes = falling_zerocross(lfootctrv-thre_fall)
-upframes = rising_zerocross(lfootctrv-thre_up)
-print(fallframes+roi0)
-for strike in fallframes:
+for strike in lfallframes:
     vicon.CreateAnEvent(subjectname, 'Left', 'Foot Strike', strike+roi0, 0.0 )
-for fr in upframes:
+for fr in lupframes:
     vicon.CreateAnEvent(subjectname, 'Left', 'Foot Off', fr+roi0, 0.0 )
-
 
 
 
