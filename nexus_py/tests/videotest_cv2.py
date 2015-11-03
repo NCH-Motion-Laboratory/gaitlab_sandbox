3b76409640a1dcf5bfcbecadd76caf9c60da1e78
Jussi Nurminen <jnu@iki.fi>
2015-11-03 16:33:27 +0200

    simple video test (animate w/ matplotlib)

--- nexus_py/tests/videotest_cv2.py
+++ nexus_py/tests/videotest_cv2.py
@@ -8,41 +8,47 @@ Read video using cv2, display frames using matplotlib
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
+import matplotlib.animation as animation
 import cv2
 
 vidfile = "C://Users//HUS20664877//Desktop//Vicon//vicon_data//test//Verrokki6v_IN//2015_10_22_girl6v_IN//2015_10_22_girl6v_IN57.59875835.20151022144140.avi"
 cap = cv2.VideoCapture(vidfile)
 
-plt.figure()
 
 frames = []
 
 while True:
     ret, frame = cap.read()
     if ret and cap.isOpened():
-        plt.imshow(frame)
         frames.append(frame)
     else:
         break
 
-sys.exit()
+fig = plt.figure()
 
+im = plt.imshow(frames[0])
 
+def animate(i):
+    #for fr in frames:
+    im.set_data(frames[i])
+    return [im]
 
+def init():
+    im = plt.imshow(frames[0])
+    return [im]
 
+ani = animation.FuncAnimation(fig, animate, range(0,len(frames)), init_func=init, interval=25, blit=True)
 
+plt.draw()
 
-
-
-while(cap.isOpened()):
-    ret, frame = cap.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+sys.exit()   
+         
+axprev = plt.axes([
+0.7, 0.05, 0.1, 0.075])
+axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
+bnext = Button(axnext, 'Next')
+bnext.on_clicked(playonce)
+bprev = Button(axprev, 'Previous')
+#bprev.on_clicked(callback.prev)
 
