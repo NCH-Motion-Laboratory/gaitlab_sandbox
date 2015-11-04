387742dea4f3e2895805647e69b8d91277228693
Jussi Nurminen <jnu@iki.fi>
2015-11-04 10:31:13 +0200

    wip

--- nexus_py/tests/videotest_cv2.py
+++ nexus_py/tests/videotest_cv2.py
@@ -5,44 +5,34 @@ Created on Mon Nov 02 10:57:31 2015
 Read video using cv2, display frames using matplotlib
 """
 
+from __future__ import print_function
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
 import cv2
 
-vidfile = "C://Users//HUS20664877//Desktop//Vicon//vicon_data//test//Verrokki6v_IN//2015_10_22_girl6v_IN//2015_10_22_girl6v_IN57.59875835.20151022144140.avi"
+# laptop
+vidpath = "C://Users//HUS20664877//Desktop//Vicon//vicon_data//test//Verrokki6v_IN//2015_10_22_girl6v_IN//"
+# capture workstation
+vidpath = "C://Users//Vicon123//Desktop//"
+vidfile = vidpath + "2015_10_22_girl6v_IN57.59875835.20151022144140.avi"
 cap = cv2.VideoCapture(vidfile)
+frames = []
+ret = True
+if cap.isOpened():
+    while ret:
+        ret, frame = cap.read()
+        frames.append(frame)
+nframes = len(frames)
+print('read',nframes,'frames')
 
 plt.figure()
+im = plt.imshow(frames[0])
 
-frames = []
+for fr in frames:
+    im.set_data(fr)
+    plt.draw()
+    
+    
 
-while True:
-    ret, frame = cap.read()
-    if ret and cap.isOpened():
-        plt.imshow(frame)
-        frames.append(frame)
-    else:
-        break
-
-sys.exit()
-
-
-
-
-
-
-
-
-while(cap.isOpened()):
-    ret, frame = cap.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-
+    
