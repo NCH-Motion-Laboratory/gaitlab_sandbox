5100c97f00ad8a48e70b0940d0b6a8db52bc0e6d
Jussi Nurminen <jnu@iki.fi>
2016-03-14 11:34:40 +0200

    smart nexus plotter
    Plot kinematics/kinetics/EMG as available

--- gaitplotter/nexus_kinematicsonly_emgplot.py
+++ gaitplotter/nexus_kinematicsonly_emgplot.py
@@ -4,6 +4,7 @@ Created on Tue Apr 28 11:37:51 2015
 
 Kinematics only -EMG plot from Nexus (no kinetics) + PDF.
 L/R sides on separate plots.
+If no EMG, do one page kinematics/kinetics instead.
 
 @author: Jussi
 """
@@ -12,21 +13,37 @@ L/R sides on separate plots.
 from gp.plot import gaitplotter
 import gp.layouts
 
-plotheightratios = [3,2,2,2,2,2]
-pdf_prefix = 'Kinematics_EMG_'
-maintitleprefix='Kinematics-EMG plot for '
-
-for side in ['L', 'R']:
+def do_plot(emg=True):
+    
     gplotter = gaitplotter()
     gplotter.open_nexus_trial()
-    plotvars = gp.layouts.kinematics_emg(side)
-    gplotter.read_trial(plotvars)
-    
     trialname = gplotter.trial.trialname
-    maintitle = maintitleprefix + trialname + ' ('+side+')'
-    maintitle = maintitle + '\n' + gplotter.get_emg_filter_description()
-    
-    gplotter.plot_trial(plotheightratios=plotheightratios, maintitle=maintitle)
-    gplotter.create_pdf(pdf_name=pdf_prefix+trialname+'_'+side+'.pdf')
-    gplotter.show()
 
+    if emg:
+        plotheightratios = [3,2,2,2,2,2]
+        pdf_prefix = 'Kinematics_EMG_'
+        maintitleprefix='Kinematics-EMG plot for '
+        for side in ['L', 'R']:
+            plotvars = gp.layouts.kinematics_emg(side)
+            gplotter.read_trial(plotvars)
+            maintitle = maintitleprefix + trialname + ' ('+side+')'
+            maintitle = maintitle + '\n' + gplotter.get_emg_filter_description()
+            gplotter.plot_trial(plotheightratios=plotheightratios, maintitle=maintitle)
+            gplotter.create_pdf(pdf_name=pdf_prefix+trialname+'_'+side+'.pdf')
+            gplotter.show()
+            
+    else:
+        plotvars = gp.layouts.std_kinall
+        pdf_prefix = 'Kinematics_kinetics_'
+        maintitleprefix = 'Kinematics/kinetics plot for '
+        maintitle = maintitleprefix + trialname
+        gplotter.read_trial(plotvars)
+        gplotter.plot_trial(maintitle=maintitle)
+        gplotter.create_pdf(pdf_name=pdf_prefix+trialname+'_'+side+'.pdf')
+        gplotter.show()
+
+
+if __name__ == '__main__':
+    do_plot(emg=False)
+    
+    
--- gaitplotter/nexus_kinetics_emgplot.py
+++ gaitplotter/nexus_kinetics_emgplot.py
@@ -3,6 +3,7 @@
 Created on Tue Apr 28 11:37:51 2015
 
 Kinetics-EMG plot from Nexus.
+If no EMG data, kinetics/kinematics plot instead.
 
 @author: Jussi
 """
@@ -10,32 +11,39 @@ Kinetics-EMG plot from Nexus.
  
 from gp.plot import gaitplotter
 import gp.layouts
-from gp.misc import error_exit
 
-plotheightratios = [3,2,2,3,2,2,2,3]
-pdf_prefix = 'Kinetics_EMG_'
-maintitleprefix='Kinetics-EMG plot for '
 gplotter = gaitplotter()
 
-# need to open trial before detecting side
-gplotter.open_nexus_trial()
-side = gplotter.trial.kinetics_side
-if not side:
-    error_exit('Forceplate strike not detected, no kinetics available. Check that the subject weight '+
-                'is correctly entered and there is a clean forceplate strike.')
+def do_plot(emg=True):
+
+    # need to open trial before detecting side
+    gplotter.open_nexus_trial()
+    side = gplotter.trial.kinetics_side
+    if emg:
+        plotvars = gp.layouts.kinetics_emg(side)
+        plotheightratios = [3,2,2,3,2,2,2,3]
+        pdf_prefix = 'Kinetics_EMG_'
+        maintitleprefix='Kinetics-EMG plot for '
+    else:
+        plotvars = gp.layouts.std_kinall
+        plotheightratios = None
+        pdf_prefix = 'Kinetics_kinematics_'
+        maintitleprefix='Kinetics/kinematics plot for '
     
-plotvars = gp.layouts.kinetics_emg(side)
+    # choose EMG variables according to side
+    gplotter.read_trial(plotvars)
+    
+    trialname = gplotter.trial.trialname
+    maintitle = maintitleprefix + trialname
+    if emg:
+        maintitle = maintitle + '\n' + gplotter.get_emg_filter_description()
+    
+    gplotter.plot_trial(plotheightratios=plotheightratios, maintitle=maintitle)
+    gplotter.create_pdf(pdf_prefix=pdf_prefix)
+   
+    gplotter.show()
 
-# choose EMG variables according to side
-gplotter.read_trial(plotvars)
-
-trialname = gplotter.trial.trialname
-maintitle = maintitleprefix + trialname
-maintitle = maintitle + '\n' + gplotter.get_emg_filter_description()
-
-gplotter.plot_trial(plotheightratios=plotheightratios, maintitle=maintitle)
-gplotter.create_pdf(pdf_prefix=pdf_prefix)
-
-gplotter.show()
-
+if __name__ == '__main__':
+    do_plot(emg=False)
+    
 
