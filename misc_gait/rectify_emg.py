70a83788374f0879071549de8bd3cd90936eac06
Nurminen Jussi <jnu@iki.fi>
2021-01-29 12:19:53 +0200

    add rectify EMG script

---
+++ misc_gait/rectify_emg.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Read EMG channels from Nexus. Calculate linear envelope and rectified signal.
+Downsample and write as model outputs.
+
+Based on extractEMG.py by Ned Pires.
+
+@author: Jussi (jnu@iki.fi)
+
+requires: gaitutils, numpy, scipy
+"""
+
+# %% init
+
+import numpy as np
+import scipy
+from gaitutils import nexus
+
+
+# define parameters
+HPF = 5  # high pass frequency
+LPF = 40  # low pass frequency
+BUTTER_ORDER = 4  # filter order
+emg_devname = 'Myon EMG'  # name of the EMG device in Nexus
+
+# read EMG data
+vicon = nexus.viconnexus()
+emgdata = nexus._get_emg_data(vicon)['data']
+meta = nexus._get_metadata(vicon)
+emgrate = meta['analograte']
+framerate = meta['framerate']
+subject = meta['name']
+nframes = meta['length']
+
+# get roi length
+startFrame = vicon.GetTrialRegionOfInterest()[0]
+endFrame = vicon.GetTrialRegionOfInterest()[1]
+roi_length = endFrame - startFrame + 1
+
+# apply hpf
+b_HPF, a_HPF = scipy.signal.butter(BUTTER_ORDER, HPF * 2 / emgrate, 'high', analog=False)
+emg_hpf = {
+    chname: scipy.signal.filtfilt(b_HPF, a_HPF, chdata) for chname, chdata in emgdata.items()
+}
+
+# remove dc
+emg_dc_offset = {
+    emgname: emg_hpf[emgname] - np.mean(emg_hpf[emgname]) for emgname in emgdata.keys()
+}
+
+# rectify
+emg_rectified = {emgname: np.abs(emg_dc_offset[emgname]) for emgname in emgdata.keys()}
+
+# apply lpf
+b_LPF, a_LPF = scipy.signal.butter(BUTTER_ORDER, LPF * 2 / emgrate, 'low', analog=False)
+emg_rectified_lpf = {
+    emgname: scipy.signal.filtfilt(b_LPF, a_LPF, emg_rectified[emgname])
+    for emgname in emgdata.keys()
+}
+
+# downsample
+emg_rectified_ds = {
+    emgname + '_Rectified': scipy.signal.resample(emg_rectified[emgname], nframes)
+    for emgname in emgdata
+}
+emg_linearenvelope_ds = {
+    emgname + '_LinearEnvelope': scipy.signal.resample(emg_rectified_lpf[emgname], nframes)
+    for emgname in emgdata
+}
+
+# %% write the processed EMG as model outputs
+
+# create the new model outputs in Nexus
+fullModelOutputList = vicon.GetModelOutputNames(subject)
+newModelOutputNames = emg_rectified_ds.keys() + emg_linearenvelope_ds.keys()
+
+createModelOutput = [
+    vicon.CreateModelOutput(
+        subject,
+        emgname,
+        'EMG',
+        {'EMG', 'EMG', 'EMG'},
+        {'Electric Potential', 'Electric Potential', 'Electric Potential'},
+    )
+    for emgname in newModelOutputNames
+    if emgname not in fullModelOutputList
+]
+
+exists = [True] * nframes
+
+for chname, data in emg_rectified_ds.items():
+    vicon.SetModelOutput(
+        subject, chname, [emg_rectified_ds[chname]], exists
+    )
+
+for chname, data in emg_linearenvelope_ds.items():
+    vicon.SetModelOutput(
+        subject,
+        chname,
+        [emg_linearenvelope_ds[chname]],
+        exists,
+    )
