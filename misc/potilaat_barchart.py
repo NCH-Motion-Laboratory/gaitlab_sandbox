7d8181f6eb44c50bc328334cac5cc292e4d741f1
Nurminen Jussi <jnu@iki.fi>
2022-02-23 14:54:15 +0200

    py3 update

--- misc/potilaat_barchart.py
+++ misc/potilaat_barchart.py
@@ -8,7 +8,6 @@ Created on Thu Feb 09 11:17:51 2017
 
 import matplotlib.pyplot as plt
 import numpy as np
-from collections import OrderedDict
 
 FONTSIZE = 15
 LEGEND_FONTSIZE = 12
@@ -16,7 +15,6 @@ UPK_LEGEND = 'Ulkopaikkakuntalaisten osuus\n(vuoden 2021 tietoa ei Apotista saat
 
 # set global font size
 plt.rc('font', size=FONTSIZE)
-
 
 
 def autolabel(rects, label_zeros=True):
@@ -52,17 +50,18 @@ yrs_plot = range(2015, 2022)
 # kelkkamittaus
 # hengityskaasu
 
-data = OrderedDict()  # bars will be plotted in the order they are given here
-data[u'Kävelyanalyysi'] = dict()
-data[u'Kävelyanalyysi']['total'] = [46, 45, 50, 67, 93, 137, 101, 124, 87, 100]
-data[u'Kävelyanalyysi']['ulko'] = [0, 0, 0, 0, 12, 23, 29, 33, 25, 0]
+data = dict()  # bars will be plotted in the order they are given here
+data['Kävelyanalyysi'] = dict()
+data['Kävelyanalyysi']['total'] = [46, 45, 50, 67, 93, 137, 101, 124, 87, 100]
+data['Kävelyanalyysi']['ulko'] = [0, 0, 0, 0, 12, 23, 29, 33, 25, 0]
 
-data[u'Kliiniset mittaukset'] = dict()
-data[u'Kliiniset mittaukset']['total'] = [46, 45, 50, 67, 93, 137, 101, 124, 94, 160]
-data[u'Kliiniset mittaukset']['ulko'] = [0, 0, 0, 0, 12, 23, 29, 33, 25, 0]
+data['Kliiniset mittaukset'] = dict()
+data['Kliiniset mittaukset']['total'] = [
+    46, 45, 50, 67, 93, 137, 101, 124, 94, 160]
+data['Kliiniset mittaukset']['ulko'] = [0, 0, 0, 0, 12, 23, 29, 33, 25, 0]
 
-data[u'Laitteistetut lihasvoimamittaukset'] = dict()
-data[u'Laitteistetut lihasvoimamittaukset']['total'] = [
+data['Laitteistetut lihasvoimamittaukset'] = dict()
+data['Laitteistetut lihasvoimamittaukset']['total'] = [
     55,
     41,
     47,
@@ -74,41 +73,44 @@ data[u'Laitteistetut lihasvoimamittaukset']['total'] = [
     23,
     24,
 ]
-data[u'Laitteistetut lihasvoimamittaukset']['ulko'] = [0, 0, 0, 0, 14, 20, 10, 3, 14, 0]
+data['Laitteistetut lihasvoimamittaukset']['ulko'] = [
+    0, 0, 0, 0, 14, 20, 10, 3, 14, 0]
 
-data[u'Painejakaumamittaukset'] = dict()
-data[u'Painejakaumamittaukset']['total'] = [0, 0, 0, 0, 77, 135, 105, 150, 162, 169]
-data[u'Painejakaumamittaukset']['ulko'] = [0, 0, 0, 0, 10, 25, 32, 36, 25, 0]
+data['Painejakaumamittaukset'] = dict()
+data['Painejakaumamittaukset']['total'] = [
+    0, 0, 0, 0, 77, 135, 105, 150, 162, 169]
+data['Painejakaumamittaukset']['ulko'] = [0, 0, 0, 0, 10, 25, 32, 36, 25, 0]
 
-data[u'EMG-mittaukset'] = dict()
-data[u'EMG-mittaukset']['total'] = [0, 0, 0, 0, 0, 114, 93, 106, 83, 100]
-data[u'EMG-mittaukset']['ulko'] = [0, 0, 0, 0, 0, 23, 28, 33, 25, 0]
+data['EMG-mittaukset'] = dict()
+data['EMG-mittaukset']['total'] = [0, 0, 0, 0, 0, 114, 93, 106, 83, 100]
+data['EMG-mittaukset']['ulko'] = [0, 0, 0, 0, 0, 23, 28, 33, 25, 0]
 
-data[u'Kävelyanalyysin viiteaineisto'] = dict()
-data[u'Kävelyanalyysin viiteaineisto']['total'] = [0, 0, 0, 0, 0, 21, 4, 36, 0, 0]
-data[u'Kävelyanalyysin viiteaineisto']['ulko'] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+data['Kävelyanalyysin viiteaineisto'] = dict()
+data['Kävelyanalyysin viiteaineisto']['total'] = [
+    0, 0, 0, 0, 0, 21, 4, 36, 0, 0]
+data['Kävelyanalyysin viiteaineisto']['ulko'] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-data[u'Yläraaja'] = dict()
-data[u'Yläraaja']['total'] = [0, 0, 0, 0, 0, 0, 0, 0, 7, 6]
-data[u'Yläraaja']['ulko'] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+data['Yläraaja'] = dict()
+data['Yläraaja']['total'] = [0, 0, 0, 0, 0, 0, 0, 0, 7, 6]
+data['Yläraaja']['ulko'] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-data[u'Hengityskaasu'] = dict()
-data[u'Hengityskaasu']['total'] = [0, 0, 0, 0, 0, 0, 0, 0, 7, 4]
-data[u'Hengityskaasu']['ulko'] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+data['Hengityskaasu'] = dict()
+data['Hengityskaasu']['total'] = [0, 0, 0, 0, 0, 0, 0, 0, 7, 4]
+data['Hengityskaasu']['ulko'] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-data[u'Kelkkamittaukset'] = dict()
-data[u'Kelkkamittaukset']['total'] = [0, 0, 0, 0, 0, 0, 0, 0, 10, None]
-data[u'Kelkkamittaukset']['ulko'] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+data['Kelkkamittaukset'] = dict()
+data['Kelkkamittaukset']['total'] = [0, 0, 0, 0, 0, 0, 0, 0, 10, 0]
+data['Kelkkamittaukset']['ulko'] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 # check length
 for var, di in data.items():
     for name, li in di.items():
         if len(li) != len(yrs):
-            raise RuntimeError(u'invalid data length for %s/%s' % (var, name))
+            raise RuntimeError('invalid data length for %s/%s' % (var, name))
 
 # crop data to desired years
 # assumes continuous ranges
-start_ind, end_ind = yrs.index(yrs_plot[0]), yrs.index(yrs_plot[-1]) 
+start_ind, end_ind = yrs.index(yrs_plot[0]), yrs.index(yrs_plot[-1])
 for var, di in data.items():
     for name, li in di.items():
         di[name] = li[start_ind:end_ind+1]
@@ -163,7 +165,8 @@ locs, labels = plt.xticks()
 locs = [x - 0.25 for x in locs]
 plt.xticks(locs, yrs_plot)
 
-fn = 'tilastot_%d.png' % max(yrs_plot)
+this_year = max(yrs_plot)
+fn = f'tilastot_{this_year}.png'
 fig.set_size_inches(18.5, 10.5)
 plt.savefig(fn, dpi=200)
-print('wrote %s' % fn)
+print(f'wrote {fn}')
